7d326243b104e9f8345ba4fc54249154673c6e9a
Richu Jain <34774248+richujain@users.noreply.github.com>
2019-09-28 14:16:52 -0400

    Trying to implement login

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php
+<?php error_reporting(0);
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -11,4 +11,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 //echo "Connected successfully";
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM tbl_login WHERE username='$username' AND password='$password'";
+  $result = mysqli_query($conn, $sql);
+  echo "result is " . $result;
+  if ($row = mysqli_fetch_assoc($result)) {
+    echo "You are logged in!   " .$sql;
+
+  } else {
+    echo "Your username or password is incorrect!   " .$sql;
+  }
 ?>
